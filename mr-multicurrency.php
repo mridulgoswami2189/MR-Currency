cf4b01e419e8349d0f3c2a636545ffe406c9d402
MGoswami <mgoswami2189@gmail.com>
2025-08-26 13:36:05 +0530

    Initialising plugin activation file

---
+++ mr-multicurrency.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: MR Multi‑Currency (Geo + Markup)
+ * Description: Detects user geo-location and switches currency with auto/manual rates and per-currency markup. (Milestone 1: bootstrap + settings.)
+ * Author: Mridul & Rohan
+ * Version: 0.1.0
+ * Requires at least: 6.0
+ * Requires PHP: 8.3
+ * Text Domain: mr-multicurrency
+ * Domain Path: /languages
+ * WC tested up to: 9.0
+ */
