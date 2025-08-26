c535935a10ad89693a73cf5d03c11d6cfa3b81ff
MGoswami <mgoswami2189@gmail.com>
2025-08-26 13:44:38 +0530

    Remove useless description

--- mr-multicurrency.php
+++ mr-multicurrency.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Plugin Name: MR Multi‑Currency (Geo + Markup)
- * Description: Detects user geo-location and switches currency with auto/manual rates and per-currency markup. (Milestone 1: bootstrap + settings.)
+ * Plugin Name: MR Multi‑Currency
+ * Description: Detects user geo-location and switches currency with auto/manual rates and per-currency markup.
  * Author: Mridul & Rohan
  * Version: 0.1.0
  * Requires at least: 6.0
